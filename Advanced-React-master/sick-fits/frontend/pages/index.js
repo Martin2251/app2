1f4519509a0862ee7cef6bb28c715bddfea371a4
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-12-06 07:32:30 +0000

    document

--- Advanced-React-master/sick-fits/frontend/pages/index.js
+++ Advanced-React-master/sick-fits/frontend/pages/index.js
@@ -2,9 +2,9 @@ import Page from "../components/Page"
 
 const Home = () => {
     return(
-        <Page cool="Heck ya!">
+        <>
             <h1>welcome home!</h1>
-        </Page>
+        </>
     )
 }
 export default Home
