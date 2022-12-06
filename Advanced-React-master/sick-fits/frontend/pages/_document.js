2dfe45439082b953ca414c35baed6f4eb1a33206
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-12-06 07:44:58 +0000

    _document

--- Advanced-React-master/sick-fits/frontend/pages/_document.js
+++ Advanced-React-master/sick-fits/frontend/pages/_document.js
@@ -1,5 +1,14 @@
-import Document from 'next/document'
+import Document, { Html } from 'next/document'
+import {HTML,Head, NextScript, Main} from 'next/document'
 
 export default class MyDocument extends Document{
-
+render () {
+    <Html lang='en'>
+        {/* <Head></Head> */}
+        <body>
+        <Main  />
+        <NextScript  />
+        </body>
+    </Html>
+}
 }
