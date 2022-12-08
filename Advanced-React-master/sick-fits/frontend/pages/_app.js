9c4a072cbc94e8e1bd6a1b1c8b3aaf2f767bf68e
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-12-08 07:44:29 +0000

    nprogress

--- Advanced-React-master/sick-fits/frontend/pages/_app.js
+++ Advanced-React-master/sick-fits/frontend/pages/_app.js
@@ -1,4 +1,15 @@
-import Page from "../components/Page"
+import Page from "../components/Page";
+import NProgress from "nprogress";
+import Router from "next/dist/next-server/server/router";
+import "../components/styles/nprogress.css";
+// TODO: SWAP WITH OUR OWN
+import 'nprogress/nprogress.css';
+
+
+Router.events.on('routeChangeStart', () =>NProgress.start());
+Router.events.on('routeChangeComplete', () =>NProgress.done());
+Router.events.on('routeChangeError', () =>NProgress.done());
+
 const MyApp = ({Component,pageProps}) => {
     return(
         <Page>
