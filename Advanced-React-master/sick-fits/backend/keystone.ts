339a9086797a364d072bbfd03e86e1a753a91f95
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-12-10 18:43:38 +0000

    db adapter

--- Advanced-React-master/sick-fits/backend/keystone.ts
+++ Advanced-React-master/sick-fits/backend/keystone.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import {config, createSchema} from '@keystone-next/keystone/schema'
 
 //get our database url 
 const databaseURL = process.env.DATABASE_URL
@@ -6,5 +7,19 @@ const databaseURL = process.env.DATABASE_URL
 //create session config to authentic users
 
 const sessionConfig = {
-    maxAge: 60 * 60 * 24 * 360 // How long should they stay signed in for?
-}
+    maxAge: 60 * 60 * 24 * 360, // How long should they stay signed in for?
+    secret: process.env.COOKIE_SECRET,
+};
+
+export default config ({
+    server:{
+        cors:{
+            origin: [process.env.FRONTEND_URL],
+            credentials:true
+        }
+    },
+    db:{
+        adapter:'mongoose',
+        url:da
+    }
+})
